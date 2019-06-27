3e38eae0bb4c085b9e2c516942e674f9075008d9
Stephen Lang <stephenlang91@gmail.com>
2019-06-26 23:00:20 -0400

    issue with /game persists

--- main.rb
+++ main.rb
@@ -7,3 +7,15 @@ enable :sessions
 get '/' do 
     erb :index
 end
+
+post '/' do 
+    session[:game] = Game.new
+    redirect to('/game')
+end
+
+get '/game' do 
+    erb :game, :locals => {:secret_word => session[:game].secret_word,
+                            :wrong_answers => session[:game].wrong_answers,
+                            :code => session[:game].code,
+                            :guessed_letters => sessions[:game].guessed_letters}
+end
